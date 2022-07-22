b2bd0cf8139bd0e28832ba76c99066008c4e9879
PC <507195389@qq.com>
2022-07-22 11:24:34 +0800

    新增配置

---
+++ configtest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+#@Time : 2022/07/22 11:12
+#@Author : Administrator
+#@Name : PC
+#@FileName : configtest.py
+
---
+++ utils/rs.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#@Time : 2022/07/22 11:05
+#@Author : Administrator
+#@Name : PC
+#@FileName : rs.py
+
+import os
+
+
+print(os.path.basename())
